693b5d41e47ad3d9e336213081bd6b964e93b8a3
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-06-21 22:57:48 +0300

    Create transformation.java
    transformation double to integer

---
+++ transformation.java
@@ -0,0 +1,14 @@
+package answerquizquestions;
+
+public class transformation {
+	public static void main(String[] args) {
+		
+		double num = 50;
+		int num1 = (int)num;
+		
+		System.out.println(num);
+		System.out.println(num1);
+		
+	}
+
+}
